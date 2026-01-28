8f686d56b2f067c5434a0d27c251c6d4098ba6ba
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-28 20:37:22 +0530

    create gaurd service for  admin and user in service folder

---
+++ src/service/guard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Navigate,useLocation } from "react-router-dom";
+import ApiService from './apiService';
+
+
+//If the user is logged in, it allows access to the page
+//If the user is NOT logged in, it redirects them to the login page
+
+export const ProtectedRoute =({element:Component})=>{
+
+                                      
+                                    // useLocation() gets the current URL location  // This helps us remember where the user was trying to go
+  const location = useLocation();
+                                      /*
+                                        ApiService.isAdmin():
+                                        - returns true if user is logged in
+                                        - returns false if user is NOT logged in
+                                      */
+  return ApiService.isAdmin()?(
+                                    // If user IS Admin:  // allow access to the requested component/page
+    Component
+  ):(
+                                    // If user is NOT Admin:  // redirect them to the login page  // "replace" prevents back button returning to protected page // "state" saves the previous location for redirect after login
+    <Navigate to="/login" replace state={{from:location}}/>
+  );
+}
+
+
+
+
+export const AdminRoute =({element:Component})=>{
+
+                                      
+                                    // useLocation() gets the current URL location  // This helps us remember where the user was trying to go
+  const location = useLocation();
+                                      /*
+                                        ApiService.isAdmin():
+                                        - returns true if user is logged in
+                                        - returns false if user is NOT logged in
+                                      */
+  return ApiService.isAdmin()?(
+                                    // If user IS Admin:  // allow access to the requested component/page
+    Component
+  ):(
+                                    // If user is NOT Admin:  // redirect them to the login page  // "replace" prevents back button returning to protected page // "state" saves the previous location for redirect after login
+    <Navigate to="/login" replace state={{from:location}}/>
+  );
+}
+
+
