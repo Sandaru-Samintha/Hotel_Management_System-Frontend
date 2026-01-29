af554d4366ae19526672e59a9a6b7a467fa4d754
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-29 20:19:17 +0530

    Create the Home Page

---
+++ src/component/home/HomePage.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+
+    <div className='home'>
+      
+      {/*Header / Banner Room Section */}
+      <section>
+        <header className='header-banner'>
+          <img src='./assets/images/hotel-image.jpg' alt="Nexus Haven Hotel" className='header-image'/>
+          <div className='overlay'></div>
+          <div className='animated-texts overlay-content'>
+            <h1>
+              Welcome to <span className="nexus-color">Nexus Haven Hotel</span>
+              </h1><br/>
+              <h3>Designed for comfort. Dedicated to you.</h3>
+          </div>
+        </header>
+      </section>
+
+      {/* SEARCH/FIND AVAILABLE ROOMS SECTION */}
+      <h4><a href="/rooms" className="view-rooms-home">All Rooms</a></h4>
+
+      <h2 className="home-services">Services at  <span className="nexus-color">Nexus Haven Hotel</span></h2>
+
+      {/* SERVICE SECTION */}
+      <section className="service-section">
+        <div className="service-card">
+          <img src="./assets/images/ac.png" alt="Air Conditioning" />
+          <div className="service-details">
+            <h3 className="service-title">Air Conditioning</h3>
+              <p className="service-description">Expert on-site air conditioning solutions to keep your space cool and comfortable.</p>
+          </div>
+        </div>
+
+        <div className="service-card">
+          <img src="./assets/images/mini-bar.png" alt="Mini Bar" />
+          <div className="service-details">
+            <h3 className="service-title">Mini Bar</h3>
+              <p className="service-description">Expert on-site air conditioning solutions to keep your space cool and comfortable.</p>
+          </div>
+        </div>
+
+        <div className="service-card">
+          <img src="./assets/images/mini-bar.png" alt="Mini Bar" />
+          <div className="service-details">
+            <h3 className="service-title">Mini Bar</h3>
+              <p className="service-description">
+                We provide on-site mini bar installation, servicing, and repair for optimal performance.
+              </p>
+          </div>
+        </div>
+
+        <div className="service-card">
+          <img src="./assets/images/parking.png" alt="Parking" />
+          <div className="service-details">
+            <h3 className="service-title">Parking</h3>
+              <p className="service-description">
+              Safe and convenient parking facilities available for guests and visitors.
+            </p>
+          </div>
+        </div>
+
+        <div className="service-card">
+          <img src="./assets/images/wifi.png" alt="WiFi" />
+          <div className="service-details">
+            <h3 className="service-title">WiFi</h3>
+              <p className="service-description">
+                High-speed WiFi access to keep you connected anytime, anywhere.
+              </p>
+          </div>
+        </div>
+        
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
