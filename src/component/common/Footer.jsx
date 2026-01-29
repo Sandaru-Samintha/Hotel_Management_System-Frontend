383c99431d8007acf58bb1f9092e65bbcd268ff8
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-29 23:35:38 +0530

    Implement the display the home page

--- src/component/common/Footer.jsx
+++ src/component/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FooterComponent = () => {
   return (
     <span>
-      Nexus Haven Hotel | All Rights Reserved &copy; {new Date.getfullYear} 
+      Nexus Haven Hotel | All Rights Reserved &copy; {new Date().getFullYear()} 
     </span>
   )
 }
--- src/service/guard.js
+++ src/service/guard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate,useLocation } from "react-router-dom";
-import ApiService from './apiService';
+import ApiService from './ApiService';
 
 
 //If the user is logged in, it allows access to the page
